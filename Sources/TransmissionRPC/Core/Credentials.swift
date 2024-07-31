a2399e668caf66e654956cec773afde7cd1597cb
Otto Otsamo <ootsamo@gmail.com>
2024-08-01 00:35:29 +0300

    Add a public initializer to Credentials

--- Sources/TransmissionRPC/Core/Credentials.swift
+++ Sources/TransmissionRPC/Core/Credentials.swift
@@ -1,4 +1,9 @@
 public struct Credentials {
 	public let username: String
 	public let password: String
+
+	public init(username: String, password: String) {
+		self.username = username
+		self.password = password
+	}
 }
