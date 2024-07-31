93747695e415590f8d26dd00df96abf960d830e5
Otto Otsamo <ootsamo@gmail.com>
2024-08-01 00:07:52 +0300

    Move TorrentFilter to separate file

--- Sources/TransmissionRPC/Methods/GetTorrent.swift
+++ Sources/TransmissionRPC/Methods/GetTorrent.swift
@@ -36,32 +36,3 @@ private extension CodingKey where Self: CaseIterable {
 struct GetTorrentResponse: Decodable {
 	let torrents: [Torrent]
 }
-
-public enum TorrentFilter: ExpressibleByArrayLiteral, Encodable {
-	case all
-	case one(TorrentIdentifier)
-	case multiple([TorrentIdentifier])
-	case recentlyActive
-
-	public init(arrayLiteral elements: TorrentIdentifier...) {
-		if elements.count == 1 {
-			self = .one(elements[0])
-		} else {
-			self = .multiple(elements)
-		}
-	}
-
-	public func encode(to encoder: Encoder) throws {
-		var container = encoder.singleValueContainer()
-		switch self {
-		case .all:
-			break
-		case .one(let identifier):
-			try container.encode(identifier)
-		case .multiple(let identifiers):
-			try container.encode(identifiers)
-		case .recentlyActive:
-			try container.encode("recently-active")
-		}
-	}
-}
